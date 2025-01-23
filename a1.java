526608acaae1b868653913522e77671bb8172c93
Vedant Dhadge <107037206+dhadgevedant@users.noreply.github.com>
2025-01-23 15:43:11 +0530

    Add files via upload

---
+++ a1.java
@@ -0,0 +1,22 @@
+class a1{
+	
+	public static void main(String args[]){
+	
+		while(true)	
+			
+		System.out.println("Hello World!");
+		
+	}
+
+};
+
+/*
+	Steps: 
+	
+		1. Create a class named same as file name
+		2. Create a public static void main function inside class
+		3. Pass String args[] as parameters for main function
+		4. inside main function Print hello world by System.out.println("Hello World!");
+		5. Close the main function
+		5. Close the Class 
+*/
