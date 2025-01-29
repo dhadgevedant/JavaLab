acdd06e4378bd4f32809df8ac22932755906a2cf
Vedant Dhadge <107037206+dhadgevedant@users.noreply.github.com>
2025-01-29 13:27:12 +0530

    Add files via upload

---
+++ a3.java
@@ -0,0 +1,23 @@
+//Declaring another class
+class Sample{
+	
+	void show(){
+		
+		System.out.println("Hello World");
+	
+	}
+
+}
+
+class a3{
+
+
+	public static void main(String args[]){
+		
+		//sample class object creation
+		Sample a = new Sample();
+		a.show();
+	
+	}
+
+}
