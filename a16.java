8d1e0f58bf80ab9587d907084b910f92e2e632a8
Vedant Dhadge <107037206+dhadgevedant@users.noreply.github.com>
2025-02-05 14:20:33 +0530

    Add files via upload

---
+++ a16.java
@@ -0,0 +1,21 @@
+//short hand techniques for basic operations
+
+/*
+	a = a * b  <--->  a *= b
+	
+	same can be done with other mathematical operations as well eg: +,-,/,%
+*/
+
+class a16{
+
+	
+	public static void main(String arg[]){
+		
+		int a = 10, b = 20;
+		
+		a *= b;//which is similat to a = a * b;
+		
+		System.out.println(a);
+	}
+
+}
